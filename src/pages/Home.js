27222874f1b3f0f71d864e62c534a144fd2e9086
Talk Tweety <delbakhshis@lhprep.org>
2022-12-19 11:57:44 -0500

    home layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,31 @@ import { useLocation } from "react-router-dom";
 
 
 class Home extends Component {
+
+    topics = [{ "img": "img source", "headline": "Skin cancer","bullets": ["Covered", "non-bias", "Affecting"]}]
     render () {
     // console.log(location)
     return (
-    <p> Greetigs </p>
+    <div> 
+    <h1>Topics in the News</h1> 
+    <div class = "scrollableContainer">
+    {this.topics.map((item, index) => (
+            <div className= "card">
+            <p>{item.img}</p>
+            <h1>{item.headline}</h1>
+            {item.bullets.map((item, index) => (
+                <li>{item}</li>
+            ))}
+            <button> learn more </button>
+
+            </div>
+    ))}
+
+
+    </div>
+    </div>
     )
     }
 }
-
-
 
 export default Home;
