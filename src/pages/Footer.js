e2b0ab96fa1d3b25aedafc1826f2186b5c9bc6df
cierrabeck <cnb44@cornell.edu>
2023-01-30 16:26:49 -0500

    adding packages and modifying nav bar

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,24 +1,34 @@
 import './Footer.css';
 import React from "react";
-import { Link } from "react-router-dom";
-import graph from '../assets/graph.svg'
-import pin from '../assets/pin.svg'
-import newspaper from '../assets/newspaper.svg'
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNewspaper, faChartBar, faMap } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
 
+        const tabs = [{
+                route: "/Home",
+                icon: faNewspaper,
+                label: "Home"
+        }, {
+                route: "/Overview",
+                icon: faChartBar,
+                label: "Overview"
+        }, {
+                route: "/County",
+                icon: faMap,
+                label: "County"
+        }]
+
         return (
                 <div class="footer">
-                        <Link className="Home" type="submit" to={'/Home'}>
-                                <img src={newspaper} />
-                        </Link>
-                        <Link className="Overview" type="submit" to={'/Overview'}>
-                                <img src={graph} />
-                        </Link>
-                        <Link className="County" type="submit" to={'/County'}>
-                                <img src={pin} />
-
-                        </Link>
+                        <div class="icon-row">
+                                {tabs.map((tab, index) => (
+                                                <NavLink activeClassName="active" className="bottom-nav-link" type="submit" to={tab.route}>
+                                                                <FontAwesomeIcon className="item fa-3x" icon={tab.icon} />
+                                                </NavLink>
+                                ))}
+                        </div>
                 </div>
         )
 }
